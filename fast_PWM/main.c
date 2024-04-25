a58a7c09ae179f0a38b1f80afdc33ae8f208d18a
fdch <fedchenko@yahoo.com>
2024-04-25 12:52:34 +0400

    fast_PWM

---
+++ fast_PWM/main.c
@@ -0,0 +1,34 @@
+#include <mega16a.h>
+#include <stdint.h>
+#include <delay.h>
+
+#define PWMQ 3
+
+void init_PWM(void)
+{
+  DDRB |= 1 << PWMQ;                    // PORTB на вывод
+  TCCR0 |= (1 << WGM00) | (1 << WGM01); // Установка режима fast PWM
+  TCCR0 |= (1 << COM01);                // Установка прямого сигнала PWM
+  TCCR0 |= (1 << CS00);                 // Установка частоты без предделителя
+}
+
+void main(void)
+{
+  uint8_t i = 4;
+
+  init_PWM();
+
+  while (1)
+  {
+    for (; i < 250; i++)
+    {
+      OCR0 = i;
+      delay_ms(2);
+    }
+    for (; i > 4; i--)
+    {
+      OCR0 = i;
+      delay_ms(2);
+    }
+  }
+}
